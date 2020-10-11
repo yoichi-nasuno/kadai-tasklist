3ad58933df8d92e0d25a09d2d5c471ecbf08a542
yoich-nasuno <nyoichi223@gmail.com>
2020-10-11 03:08:53 +0000

    2020/10/11 昼前の結果create 迄出来た

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,12 +4,23 @@ class TasksController < ApplicationController
   end 
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = 'Taskが正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Taskが投稿されませんでした'
+      render :new
+    end 
   end
 
   def edit
@@ -21,5 +32,11 @@ class TasksController < ApplicationController
   def destroy
   end
   
+  private
+  
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content)
+  end 
   
 end
