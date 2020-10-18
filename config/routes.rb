540eb369ce5d268d7170c4616e2c4796abbc579e
yoich-nasuno <nyoichi223@gmail.com>
2020-10-18 07:56:16 +0000

    user signup not finished yet

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'toppages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
-  
   resources :tasks
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
 end
