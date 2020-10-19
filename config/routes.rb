af6e36d9555e926a5134ecea71850fac075d4bf9
yoich-nasuno <nyoichi223@gmail.com>
2020-10-19 08:24:01 +0000

    logout後のindexに戻るのが解決した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  root to: 'toppages#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   resources :tasks
