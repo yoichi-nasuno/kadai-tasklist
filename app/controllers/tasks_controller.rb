d21f12ffa1570589fe707a85d0810a57a995a8ab
yoich-nasuno <nyoichi223@gmail.com>
2020-10-25 11:25:41 +0000

    使用不可能状態での中断

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy]
+    
   def index
-    @tasks = Task.all
+    @task = Task.all
   end 
 
   def show
@@ -11,20 +13,18 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
-
+  
   def create
-    @task = Task.new(task_params)
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Taskが登録されました'
+      flash[:success] = 'タスクを登録しました。'
       redirect_to root_url
     else
-      @tasks = curret_user.tasks.order(id: :desc).page(params[:page])
-      flash.now[:danger] = 'Taskが登録されませんでした'
-      render 'tasks/index'
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).page(10)
+      flash.now[:danger] = 'タスクの登録に失敗しました。'
+      render 'toppages/index'
     end
-  end 
-  
+  end
 
   def edit
     @task = Task.find(params[:id])
@@ -43,18 +43,23 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
-    
-    flash[:success] = 'タスクは正常に削除されました'
-    redirect_to tasks_url
-  end
+    flash[:success] = 'Taskを削除しました'
+    redirect_back(fallback_location: root_path)
+  end 
   
   private
-  
+    
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end 
+  end 
+
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:status, :content)
+    params.require(:task).permit(:status, :content,)
   end 
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :status, presence: true, length: { maximum: 15}
   validates :content, presence: true, length: { maximum: 255}
-  validates :status, presence: true, length: { maximum: 10}
 end
