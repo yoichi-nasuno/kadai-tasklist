966c808551e138621b74dca239391ea84fe3e7c8
yoich-nasuno <nyoichi223@gmail.com>
2020-10-29 13:05:42 +0000

    課題再提出3回目

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
       # flash[:success] = 'ユーザを登録しました。'
       redirect_to login_url
     else
-      # flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
   end
