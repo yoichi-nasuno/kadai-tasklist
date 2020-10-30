5abf1ddbca22a8b0aa17ffabe80129b308bb7ef5
yoich-nasuno <nyoichi223@gmail.com>
2020-10-30 12:44:51 +0000

    再提出6回目

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,11 +16,11 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      # flash[:success] = 'タスクを登録しました。'
+      flash[:success] = 'タスクを登録しました。'
       redirect_to root_url
     else
       
-      # flash.now[:danger] = 'タスクの登録に失敗しました。'
+      flash.now[:danger] = 'タスクの登録に失敗しました。'
       render :new
     end
   end
@@ -31,17 +31,17 @@ class TasksController < ApplicationController
   def update
     
     if @task.update(task_params)
-      # flash[:success] = 'タスクは正常に更新されました'
+      flash[:success] = 'タスクは正常に更新されました'
       redirect_to @task
     else
-      # flash.now[:danger] = 'タスクは更新されませんでした'
+      flash.now[:danger] = 'タスクは更新されませんでした'
       render :edit
     end 
   end
 
   def destroy
     @task.destroy
-    # flash[:success] = 'Taskを削除しました'
+    flash[:success] = 'Taskを削除しました'
     redirect_back(fallback_location: root_path)
   end 
   
